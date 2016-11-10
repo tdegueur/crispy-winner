0d5a8081332ed309bc98312ac70e948627d785b1
Thibaud Degueurce <tdegueurce@gael.fr>
2016-11-10 17:21:46 +0100

    removed useful comments

--- src/test/java/crispy/winner/AppTest.java
+++ src/test/java/crispy/winner/AppTest.java
@@ -1,18 +1,11 @@
 package crispy.winner;
 
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest
 {
-   /**
-    * Rigourous Test :-)
-    */
    @Test
    public void testApp()
    {
