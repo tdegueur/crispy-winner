588880e45e5d68716432307e063241d621ce88a8
Thibaud Degueurce <tdegueurce@gael.fr>
2016-11-14 15:48:57 +0100

    corrected typo in App.java

--- src/main/java/crispy/winner/App.java
+++ src/main/java/crispy/winner/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "PECAN PIE WITH MAPE SYRUP" );
+        System.out.println( "PECAN PIE WITH MAPLE SYRUP" );
     }
 }
